9634392c688d9c51ec9f960147ecf3f4dd5704f0
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-09-26 01:59:40 +0300

    add app_assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,17 @@
+class Assets {
+  Assets._();
+  
+  static const String imagesForgetPassword = "assets/images/forgetPassword.png";
+
+  static const String imagesLoginVector1 = "assets/images/loginVector1.svg";
+
+  static const String imagesLoginVector2 = "assets/images/loginVector2.svg";
+
+  static const String imagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  static const String imagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  static const String imagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  static const String imagesOrderSuccess = "assets/images/orderSuccess.png";
+}
