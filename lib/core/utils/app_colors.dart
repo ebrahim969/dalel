1abbcfc51667741e662161e65941aa0b210bd607
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-09-26 02:08:00 +0300

    add app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors
+{
+  static const Color primaryColor = Color(0xffC49E85);
+  static const Color backGroundColor = Color(0xffF8F4F9);
+  static const Color deepBrown = Color(0xff6B4B3E);
+  static const Color gray = Color(0xffC4C4C4);
+  static const Color deepGray = Color(0xff6F6460);
+  static const Color lightreay = Color(0xffB4B4B4);
+  static const Color black = Color(0xff000000);
+}
